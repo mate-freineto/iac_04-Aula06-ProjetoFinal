1f38f0b2f96651aee6d6b0858d9c7404ecc62060
matefreineto <matefreineto>
2024-07-09 00:54:01 -0300

    Corrige output

--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,10 @@ output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
 output "public_ip_address_web" {
-  value = azurerm_public_ip.pubIP_web.*.ip_address
+  value = "${azurerm_linux_virtual_machine.acmeVM_web.*.public_ip_address}"
 }
 output "public_ip_address_db" {
-  value = azurerm_public_ip.pubIP_db.*.ip_address
+  value = "${azurerm_linux_virtual_machine.acmeVM_db.*.public_ip_address}"
 }
 output "linux_virtual_machine_names_web" {
   value = [for s in azurerm_linux_virtual_machine.acmeVM_web : s.name[*]]
